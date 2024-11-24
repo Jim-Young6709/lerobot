2353c7e974e21930cebaede0493c7bb88eeeb985
Jiahui(Jim) Yang(Young) <jiahuiya@andrew.cmu.edu>
2024-11-24 16:16:04 -0500

    add pointnet++ module

--- lerobot/common/policies/act/configuration_act.py
+++ lerobot/common/policies/act/configuration_act.py
@@ -167,5 +167,6 @@ class ACTConfig:
         if (
             not any(k.startswith("observation.image") for k in self.input_shapes)
             and "observation.environment_state" not in self.input_shapes
+            and "observation.pcd" not in self.input_shapes
         ):
             raise ValueError("You must provide at least one image or the environment state among the inputs.")
